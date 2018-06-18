240b0d96cee71cc3049e66f3fb0555fb3c74f1df
Vlad Faust <mail@vladfaust.com>
2018-06-18 19:57:11 +0300

    feat: add CORS
    Closes #5

--- src/bin/server.cr
+++ src/bin/server.cr
@@ -11,12 +11,13 @@ Services::Logger.init(ENV["APP_ENV"] == "production" ? Logger::INFO : Logger::DE
 Services::Repository.init(ENV["DATABASE_URL"], logger)
 
 # Create handlers which will process an HTTP request sequentially
-log_handler = Prism::LogHandler.new(logger)       # Logs requests
+log_handler = Prism::LogHandler.new(logger) # Logs requests
+cors = Prism::CORS.new(allow_headers: %w(accept content-type authorization))
 auth_handler = Authentication::Handler.new        # Handles JWT authentication
 cacher = Prism::Router::SimpleCacher.new(100_000) # Caches routing, increasing its performance
 router = Router.new(cacher)
 
-handlers = [log_handler, auth_handler, router] # Let's just put them all in one array
+handlers = [log_handler, cors, auth_handler, router] # Let's just put them all in one array
 
 host = ENV["HOST"]? || "0.0.0.0"       # Get HOST environment variable or use "0.0.0.0" by default
 port = ENV["PORT"]?.try &.to_i || 5000 # ditto
